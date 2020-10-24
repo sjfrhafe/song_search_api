759c6d1353baffefb6f92fdc6ada9886b5102e1e
Fred Hafemann <frederik.hafemann@gmail.com>
2020-10-24 21:24:49 +0200

    1.0.0 beta

---
+++ magnifier.js
@@ -0,0 +1,37 @@
+const puppeteer = require('puppeteer');
+
+class Magnifier{
+  init = async () => {
+    this.browser = await puppeteer.launch({headless: true});
+  }
+
+  find = async (query) => {
+    return new Promise(async (resolve, reject) => {
+      try{
+        let page
+        let timeout = setTimeout(() => {
+          if(page) {
+            reject('timeout')
+            page.close()
+          }
+        }, 4000)
+        page = await this.browser.newPage()
+        await page.goto('https://www.youtube.com/results?search_query=' + query)
+        await page.waitForSelector('#video-title')
+        const videoData = await page.evaluate(() => {
+          return{
+            link: document.getElementById('video-title').href,
+            title: document.getElementById('video-title').title
+          }
+        })
+        clearTimeout(timeout)
+        page.close()
+        resolve(videoData)
+      }catch(e){
+        reject(e)
+      }
+    })
+  }
+}
+
+module.exports = new Magnifier();
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const package = require('./package.json')
+const magnifier = require('./magnifier')
+const app = express()
+const homepath = package.homepath || '/api'
+const fetch = require('node-fetch')
+
+magnifier.init()
+
+app.get(homepath, function (req, res) {
+  res.json({message: 'See https://github.com/sjfrhafe/song_search_api#readme for documentation'})
+})
+
+app.get(homepath + '/:query', function (req, res) {
+  let query = req.params.query
+
+  magnifier.find(query)
+  .then(data => res.json(data))
+  .catch(() => res.sendStatus(500))
+})
+
+
+app.listen(2525)
